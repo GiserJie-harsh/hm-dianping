76fb5491232ffb263b064e6ddebf21886ae894af
Czj <44752092@qq.com>
2022-12-06 09:58:34 +0800

    实现秒杀优惠券下单（非高并发）----初始版本

--- src/main/java/com/hmdp/service/impl/SeckillVoucherServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/SeckillVoucherServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.SeckillVoucher;
+import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.SeckillVoucherMapper;
 import com.hmdp.service.ISeckillVoucherService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IVoucherOrderService;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -16,5 +25,6 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SeckillVoucherServiceImpl extends ServiceImpl<SeckillVoucherMapper, SeckillVoucher> implements ISeckillVoucherService {
+
 
 }
--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,18 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -17,4 +25,39 @@ import org.springframework.stereotype.Service;
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+
+    @Autowired
+    private SeckillVoucherServiceImpl seckillVoucherService;
+
+    @Autowired
+    private RedisIdWorker redisIdWorker;
+    @Override
+    public Result seckillVoucher(Long voucherId) {
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
+            return Result.fail("活动未开始！！");
+        }
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
+            return Result.fail("活动已结束！！");
+        }
+        if(voucher.getStock() < 1){
+            return Result.fail("库存不足！！");
+        }
+        //更新数据库秒杀券的库存
+        boolean success = seckillVoucherService.update()
+                .setSql("stock = stock - 1")
+                .eq("voucher_id", voucherId)
+                .gt("stock", 0)
+                .update();
+        //创建用户订单并写入数据库
+        VoucherOrder order = new VoucherOrder();
+        long id = redisIdWorker.nextId("order");
+        UserDTO user = UserHolder.getUser();
+        order.setVoucherId(voucherId);
+        order.setId(id);
+        order.setUserId(user.getId());
+        save(order);
+        //返回订单id
+        return Result.ok(id);
+    }
 }
